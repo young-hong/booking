667d07c754b341c976b343f76311b7f003d492a6
hongqiqiang <1914588872@qq.com>
2020-11-12 16:30:23 +0800

    compatible with ie

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'babel-polyfill';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 import 'antd/dist/antd.css';
 import App from './App';
 import './i18n';
